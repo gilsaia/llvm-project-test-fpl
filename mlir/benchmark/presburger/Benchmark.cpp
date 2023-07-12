3f6cbdc3e37489b63f2ecd36d037aebd6ea1f1d9
gilsaia <794433219@qq.com>
2023-07-12 22:05:01 +0800

    [feature] Add Benchmark
    [feature] optimize for intersect
    
    [MLIR][presburger] Add benchmarks for the Presburger library
    
    This commit introduces a comprehensive set of benchmarks to measure the performance of various operations in the Presburger library. These benchmarks aim to evaluate the execution time of different operations and assess the impact of optimization techniques on the size of the constraint system.
    
    Differential Revision: https://reviews.llvm.org/D150979

---
+++ mlir/benchmark/presburger/Benchmark.cpp
@@ -0,0 +1,72 @@
+//===- Benchmark.cpp - MLIR Presburger Benchmark
+//------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements a benchmark for PresburgerRelation.
+//
+//===----------------------------------------------------------------------===//
+
+#include <benchmark/benchmark.h>
+
+#include "FPLBench.h"
+
+BENCHMARK(BM_FPLBinaryOperationCheck<false>)
+    ->Name("FPL_Union")
+    ->Setup(FPLSetupUnion)
+    ->Iterations(5);
+BENCHMARK(BM_FPLBinaryOperationCheck<true>)
+    ->Name("FPL_Union_Simplify")
+    ->Setup(FPLSetupUnion)
+    ->Iterations(5);
+
+BENCHMARK(BM_FPLBinaryOperationCheck<false>)
+    ->Name("FPL_Subtract")
+    ->Setup(FPLSetupSubtract)
+    ->Iterations(5);
+BENCHMARK(BM_FPLBinaryOperationCheck<true>)
+    ->Name("FPL_Subtract_Simplify")
+    ->Setup(FPLSetupSubtract)
+    ->Iterations(5);
+
+BENCHMARK(BM_FPLUnaryOperationCheck<false>)
+    ->Name("FPL_Complement")
+    ->Setup(FPLSetupComplement)
+    ->Iterations(70);
+BENCHMARK(BM_FPLUnaryOperationCheck<true>)
+    ->Name("FPL_Complement_Simplify")
+    ->Setup(FPLSetupComplement)
+    ->Iterations(70);
+
+BENCHMARK(BM_FPLBinaryOperationCheck<false>)
+    ->Name("FPL_Intersect")
+    ->Setup(FPLSetupIntersect)
+    ->Iterations(5);
+BENCHMARK(BM_FPLBinaryOperationCheck<true>)
+    ->Name("FPL_Intersect_Simplify")
+    ->Setup(FPLSetupIntersect)
+    ->Iterations(5);
+
+BENCHMARK(BM_FPLBinaryOperationCheck<false>)
+    ->Name("FPL_IsEqual")
+    ->Setup(FPLSetupIsEqual)
+    ->Iterations(120);
+BENCHMARK(BM_FPLBinaryOperationCheck<true>)
+    ->Name("FPL_IsEqual_Simplify")
+    ->Setup(FPLSetupIsEqual)
+    ->Iterations(120);
+
+BENCHMARK(BM_FPLUnaryOperationCheck<false>)
+    ->Name("FPL_IsEmpty")
+    ->Setup(FPLSetupIsEmpty)
+    ->Iterations(5);
+BENCHMARK(BM_FPLUnaryOperationCheck<true>)
+    ->Name("FPL_IsEmpty_Simplify")
+    ->Setup(FPLSetupIsEmpty)
+    ->Iterations(5);
+
+BENCHMARK_MAIN();
---
+++ mlir/benchmark/presburger/FPLBench.cpp
@@ -0,0 +1,246 @@
+#include "FPLBench.h"
+#include <functional>
+
+/// This function parses a single set of integer constraints from the input
+/// stream. The input format for a set is as follows:
+/// - The first line contains two integers: eqs and inEqs. They represent the
+/// number of equations and inequalities in the set, respectively.
+/// - The following lines describe each constraint equation or inequality in the
+/// set.
+///
+/// After reading the number of equations and inequalities from the input
+/// stream, the function iteratively constructs the affine expressions
+/// representing the constraints. It reads the parameters for each expression
+/// from the input stream and forms the affine expressions by multiplying the
+/// parameters with the corresponding affine dimensions or symbols. Finally, it
+/// constructs an `IntegerSet` object by creating the affine constraint
+/// expressions and indicating which constraints are equations and which are
+/// inequalities.
+IntegerSet FPLParseOneSet(std::istream &in, int &numDims, int &numSymbols,
+                          MLIRContext &ctx) {
+  int inEqs, eqs;
+  in >> eqs >> inEqs;
+  SmallVector<AffineExpr, 4> constraints;
+  SmallVector<bool, 4> isEqs;
+
+  for (int t = 0; t < (eqs + inEqs); ++t) {
+    AffineExpr equation = getAffineConstantExpr(0, &ctx);
+    for (int i = 0; i <= (numDims + numSymbols); ++i) {
+      int64_t param;
+      in >> param;
+      AffineExpr param_expr = getAffineConstantExpr(param, &ctx);
+      if (i < (numDims + numSymbols)) {
+        AffineExpr var = (i < numDims) ? getAffineDimExpr(i, &ctx)
+                                       : getAffineSymbolExpr(i - numDims, &ctx);
+        param_expr = param_expr * var;
+      }
+      equation = equation + param_expr;
+    }
+    AffineExpr rhs = getAffineConstantExpr(0, &ctx);
+    constraints.emplace_back(equation - rhs);
+    isEqs.emplace_back(t < eqs ? true : false);
+  }
+  return IntegerSet::get(numDims, numSymbols, constraints, isEqs);
+}
+
+/// This function parses a single case of a Presburger relation from the input
+/// stream. The input data format for a case is as follows:
+/// - The first line contains three integers: numDims, numSymbols, and
+/// numUnions. They represent the number of dimensions, symbols, and sets,
+/// respectively.
+/// - The following lines contain the description of each set in the union.
+PresburgerRelation FPLParseOneCase(std::istream &in, MLIRContext &ctx) {
+  int numDims, numSymbols, numUnions;
+  in >> numDims >> numSymbols >> numUnions;
+  SmallVector<IntegerSet, 4> eles;
+  for (int i = 0; i < numUnions; ++i) {
+    IntegerSet ele = FPLParseOneSet(in, numDims, numSymbols, ctx);
+    eles.emplace_back(ele);
+  }
+  IntegerRelation reli = affine::FlatAffineValueConstraints(eles[0]);
+  PresburgerRelation rel(reli);
+  for (unsigned i = 1, e = eles.size(); i < e; ++i) {
+    IntegerRelation reli = affine::FlatAffineValueConstraints(eles[i]);
+    rel.unionInPlace(reli);
+  }
+  return rel;
+}
+
+/// This function calculates the size of a given Preburger relation, which is
+/// the total number of constraints and variables. It iterates over all
+/// disjuncts in the relation and accumulates the product of the number of
+/// constraints in each disjunct and (variable count + 1) into the provided size
+/// variable.
+void FPLCountPreburgerRelationSize(PresburgerRelation &relation,
+                                   unsigned long long &size) {
+  unsigned var = relation.getNumVars();
+  for (auto ele : relation.getAllDisjuncts()) {
+    size += ele.getNumConstraints() * (var + 1);
+  }
+}
+
+std::function<void(std::string &, std::vector<PresburgerRelation> &,
+                   std::vector<PresburgerRelation> &)>
+    readFunc;
+
+std::function<void(PresburgerRelation &)> unaryExecFunc;
+
+std::function<void(PresburgerRelation &, PresburgerRelation &)> binaryExecFunc;
+
+std::string fileName;
+
+void FPLParseOneCaseOneInt(std::string &fileName,
+                           std::vector<PresburgerRelation> &sets,
+                           std::vector<PresburgerRelation> &setsNull) {
+  MLIRContext ctx(MLIRContext::Threading::DISABLED);
+  std::ifstream in(fileName);
+  size_t num;
+  in >> num;
+  for (size_t i = 0; i < num; ++i) {
+    int tmp;
+    sets.emplace_back(FPLParseOneCase(in, ctx));
+    in >> tmp;
+  }
+  in.close();
+}
+
+void FPLParseTwoCaseUseOneCase(std::string &fileName,
+                               std::vector<PresburgerRelation> &sets,
+                               std::vector<PresburgerRelation> &setsNull) {
+  MLIRContext ctx(MLIRContext::Threading::DISABLED);
+  std::ifstream in(fileName);
+  size_t num;
+  in >> num;
+  for (size_t i = 0; i < num; ++i) {
+    sets.emplace_back(FPLParseOneCase(in, ctx));
+    FPLParseOneCase(in, ctx);
+  }
+  in.close();
+}
+
+void FPLParseTwoCaseOntInt(std::string &fileName,
+                           std::vector<PresburgerRelation> &setsA,
+                           std::vector<PresburgerRelation> &setsB) {
+  MLIRContext ctx(MLIRContext::Threading::DISABLED);
+  std::ifstream in(fileName);
+  size_t num;
+  in >> num;
+  for (size_t i = 0; i < num; ++i) {
+    int tmp;
+    setsA.emplace_back(FPLParseOneCase(in, ctx));
+    setsB.emplace_back(FPLParseOneCase(in, ctx));
+    in >> tmp;
+  }
+  in.close();
+}
+
+void FPLParseThreeCaseUseTwoCase(std::string &fileName,
+                                 std::vector<PresburgerRelation> &setsA,
+                                 std::vector<PresburgerRelation> &setsB) {
+  MLIRContext ctx(MLIRContext::Threading::DISABLED);
+  std::ifstream in(fileName);
+  size_t num;
+  in >> num;
+  for (size_t i = 0; i < num; ++i) {
+    setsA.emplace_back(FPLParseOneCase(in, ctx));
+    setsB.emplace_back(FPLParseOneCase(in, ctx));
+    FPLParseOneCase(in, ctx);
+  }
+  in.close();
+}
+
+void FPLSetupUnion(const benchmark::State &state) {
+  fileName = "./PresburgerSetUnion";
+  readFunc = FPLParseThreeCaseUseTwoCase;
+  binaryExecFunc = [](PresburgerRelation &a, PresburgerRelation &b) {
+    benchmark::DoNotOptimize(a.unionSet(b));
+  };
+}
+
+void FPLSetupSubtract(const benchmark::State &state) {
+  fileName = "./PresburgerSetSubtract";
+  readFunc = FPLParseThreeCaseUseTwoCase;
+  binaryExecFunc = [](PresburgerRelation &a, PresburgerRelation &b) {
+    benchmark::DoNotOptimize(a.subtract(b));
+  };
+}
+
+void FPLSetupComplement(const benchmark::State &state) {
+  fileName = "./PresburgerSetComplement";
+  readFunc = FPLParseTwoCaseUseOneCase;
+  unaryExecFunc = [](PresburgerRelation &a) {
+    benchmark::DoNotOptimize(a.complement());
+  };
+}
+
+void FPLSetupIntersect(const benchmark::State &state) {
+  fileName = "./PresburgerSetIntersect";
+  readFunc = FPLParseThreeCaseUseTwoCase;
+  binaryExecFunc = [](PresburgerRelation &a, PresburgerRelation &b) {
+    benchmark::DoNotOptimize(a.intersect(b));
+  };
+}
+
+void FPLSetupIsEqual(const benchmark::State &state) {
+  fileName = "./PresburgerSetEqual";
+  readFunc = FPLParseTwoCaseOntInt;
+  binaryExecFunc = [](PresburgerRelation &a, PresburgerRelation &b) {
+    benchmark::DoNotOptimize(a.isEqual(b));
+  };
+}
+
+void FPLSetupIsEmpty(const benchmark::State &state) {
+  fileName = "./PresburgerSetEmpty";
+  readFunc = FPLParseOneCaseOneInt;
+  unaryExecFunc = [](PresburgerRelation &a) {
+    benchmark::DoNotOptimize(a.isIntegerEmpty());
+  };
+}
+
+template <bool useSimplify = false>
+void BM_FPLUnaryOperationCheck(benchmark::State &state) {
+  std::vector<PresburgerRelation> setsA, setsB;
+  readFunc(fileName, setsA, setsB);
+  for (auto _ : state) {
+    for (auto &rel : setsA) {
+      if (useSimplify) {
+        // TODO:call simplify
+      }
+      unaryExecFunc(rel);
+    }
+  }
+  unsigned long long relationSize = 0;
+  for (auto &rel : setsA) {
+    FPLCountPreburgerRelationSize(rel, relationSize);
+  }
+  state.counters["Constraint Size"] = relationSize;
+}
+
+template void BM_FPLUnaryOperationCheck<false>(benchmark::State &state);
+template void BM_FPLUnaryOperationCheck<true>(benchmark::State &state);
+
+template <bool useSimplify = false>
+void BM_FPLBinaryOperationCheck(benchmark::State &state) {
+  std::vector<PresburgerRelation> setsA, setsB;
+  readFunc(fileName, setsA, setsB);
+  size_t num = setsA.size();
+  for (auto _ : state) {
+    for (size_t i = 0; i < num; ++i) {
+      if (useSimplify) {
+        // TODO:call simplify
+      }
+      binaryExecFunc(setsA[i], setsB[i]);
+    }
+  }
+  unsigned long long relationSize = 0;
+  for (auto &rel : setsA) {
+    FPLCountPreburgerRelationSize(rel, relationSize);
+  }
+  for (auto &rel : setsB) {
+    FPLCountPreburgerRelationSize(rel, relationSize);
+  }
+  state.counters["Constraint Size"] = relationSize;
+}
+
+template void BM_FPLBinaryOperationCheck<false>(benchmark::State &state);
+template void BM_FPLBinaryOperationCheck<true>(benchmark::State &state);
